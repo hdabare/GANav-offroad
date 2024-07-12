2d10f2c27d9d7351d742269f09cc94e349a11cbf
Hewage Dabare <h.dabare@ecu.edu.au>
2024-07-12 14:46:01 +0800

    Rellis use custom mapping

--- mmseg/datasets/rellis_group6.py
+++ mmseg/datasets/rellis_group6.py
@@ -18,7 +18,7 @@ class RELLISDataset_Group6(CustomDataset):
     def __init__(self, **kwargs):
         super(RELLISDataset_Group6, self).__init__(
             img_suffix='.jpg',
-            seg_map_suffix='_new_our_group6.png',
+            seg_map_suffix='_our_group6.png',
             **kwargs)
         self.CLASSES = ("background", "L1 (Smooth)", "L2 (Rough)", "L3 (Bumpy)", "non-Nav (Forbidden)", "obstacle")
         self.PALETTE = [[ 108, 64, 20 ], [ 255, 229, 204 ],[ 0, 102, 0 ],[ 0, 255, 0 ],
