1dd06862ec92609bfe7f4c59560a321856b035e7
Hewage Dabare <h.dabare@ecu.edu.au>
2024-07-12 14:53:34 +0800

    add outback 300x375 config

---
+++ configs/ours/ganav_group6_outback _300x375.py
@@ -0,0 +1,26 @@
+_base_ = [
+    '../_base_/models/ours_class_att.py', '../_base_/datasets/outback_group6_300x375.py',
+    '../_base_/default_runtime.py'
+]
+
+
+optimizer = dict(type='SGD', lr=0.003, momentum=0.9, weight_decay=4e-5)
+optimizer_config = dict()
+# learning policy
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=1000)
+total_iters = 1000
+checkpoint_config = dict(by_epoch=False, interval=1000)
+evaluation = dict(interval=1000, metric='mIoU')
+
+# optimizer
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, warmup='linear',
+    warmup_iters=250,
+    warmup_ratio=1e-6,
+    by_epoch=False)
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2)
+
+
